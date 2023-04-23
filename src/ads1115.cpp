908e21546bf8f1bf99ffd05e28e08586e6809eb6
mschubi72 <github@die-schubis.de>
2023-04-23 20:31:58 +0200

    prevent false voltage readings

--- src/ads1115.cpp
+++ src/ads1115.cpp
@@ -38,34 +38,43 @@ void Ads1115::updateVoltage()
     batVoltageSum = batVoltage;
     val_01 = ads.readADC(0);
     batVoltage = ads.toVoltage(val_01);
-  /*
-    Serial.print("\t ADC1115-0: ");
-    Serial.print(batVoltage);
-    Serial.print(" V; corrected: ");
-    Serial.println(batVoltage*14.0);
-    */
+    /*
+      Serial.print("\t ADC1115-0: ");
+      Serial.print(batVoltage);
+      Serial.print(" V; corrected: ");
+      Serial.println(batVoltage*14.0);
+      */
     batVoltageSum += batVoltage;
 
-    int tempBat = 100 * (batVoltageSum*14.0/2.0);
-    status->batVoltage = tempBat/100.0;
-    //status->batVoltage = roundf(100.0f * status->batVoltage)/100.0f; 
+    int tempBat = 100 * (batVoltageSum * 14.0 / 2.0);
+    if (tempBat > 0)
+    { // sometimes Error on reading voltage
+        status->batVoltage = tempBat / 100.0;
+        // status->batVoltage = roundf(100.0f * status->batVoltage)/100.0f;
 
-    if(status->batVoltage > BAT_FULL){
-        status->batStatus = 5;
-    }else if (status->batVoltage > BAT_70)
-    {
-        status->batStatus = 4;
-    }else if (status->batVoltage > BAT_40)
-    {
-        status->batStatus = 3;
-    }else if (status->batVoltage > BAT_30)
-    {
-        status->batStatus = 2;
-    }else if (status->batVoltage > BAT_20)
-    {
-        status->batStatus = 1;
-    }else
-    {
-        status->batStatus = 0;
+        if (status->batVoltage > BAT_FULL)
+        {
+            status->batStatus = 5;
+        }
+        else if (status->batVoltage > BAT_70)
+        {
+            status->batStatus = 4;
+        }
+        else if (status->batVoltage > BAT_40)
+        {
+            status->batStatus = 3;
+        }
+        else if (status->batVoltage > BAT_30)
+        {
+            status->batStatus = 2;
+        }
+        else if (status->batVoltage > BAT_20)
+        {
+            status->batStatus = 1;
+        }
+        else
+        {
+            status->batStatus = 0;
+        }
     }
 }
